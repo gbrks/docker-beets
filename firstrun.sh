35aaf109d1f934ff1201d68388f8663c5e622f93
gbrks <gavinjbrooks@gmail.com>
2015-05-18 21:42:33 +0900

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -5,6 +5,6 @@ if [ -f /config/config.yaml ]; then
   echo "Using existing config file."
 else
   echo "Creating config.yaml from template."
-  wget -P /config/ https://raw.githubusercontent.com/smdion/docker-containers/master/beets/src/config.yaml
-  chown nobody:users /config/config.yaml
+  wget -P /config/ https://raw.githubusercontent.com/gbrks/docker-beets/master/src/config.yaml
+  chown $USERID:users /config/config.yaml
 fi
